353a7825dc8caaaa097bfaa4c718c470601b663d
Argel Omnes <argelomnes@gmail.com>
2019-12-03 11:57:13 +0800

    Responsive layout

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    theme: {
+        screens: {
+            sm: "540px",
+            md: "760px",
+            lg: "960px",
+            xl: "1280px"
+        },
+        fontFamily: {
+            display: ["Poppins", "sans-serif"],
+            sans: ["Open Sans", "sans-serif"]
+        },
+        extend: {
+            colors: {
+                darkcyan: "#00252E",
+                brandpink: "#ff52bf",
+                paleblue: "#f5faff"
+            },
+            spacing: {
+                "9": "2.1875rem"
+            },
+            fontSize: {
+                "7xl": "4.375rem"
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
